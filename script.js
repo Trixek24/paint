87f9a757fbfc2d9cc40d4eb3501dd5597073e6ec
Trixek24 <121521201+Trixek24@users.noreply.github.com>
2023-03-23 17:07:01 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,98 @@
+mouseEvent = "empty";
+
+canvas = document.getElementById("myCanvas");
+
+ctx = canvas.getContext("2d");
+
+canvas.addEventListener("mousedown",my_mousedown);
+
+function my_mousedown(e)
+{
+    color = document.getElementById("color").value;
+    width_of_line = document.getElementById("width_of_line").value;
+
+    mouseEvent= "mouseDown";
+
+
+}
+
+canvas.addEventListener("mouseleave",my_mouseleave);
+
+function my_mouseleave(e)
+{
+    mouseEvent="mouseleave";
+}
+
+canvas.addEventListener("up",my_mouseup);
+
+function my_mouseup(e)
+{
+    mouseEvent="mouseUP";
+}
+
+canvas.addEventListener("mousemove",my_mousemove);
+
+function my_mousemove(e)
+{
+    current_position_of_mouse_x = e.clientX - canvas.offsetLeft;
+    current_position_of_mouse_y = e.clientY - canvas.offsetTop;
+
+    if (mouseEvent == "mouseDown") 
+    {
+        ctx.beginPath();
+        ctx.strokestyle = color;
+        ctx.lineWidth = width_of_line;
+
+       
+        ctx.moveTo(last_position_of_x, last_position_of_y);
+
+       
+        ctx.lineTo(current_position_of_mouse_x, current_position_of_mouse_y);
+        ctx.stroke();
+
+    }
+
+    last_position_of_x = current_position_of_mouse_x;
+    last_position_of_y = current_position_of_mouse_y;
+}
+
+function clearCanvas()
+{
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+}
+
+
+canvas.addEventListener("touchstart",my_touchstart);
+
+function my_touchstart(e)
+{
+    console.log("my_touchstart");
+
+    last_position_of_x = e.touches[0].clientX - canvas.offsetLeft;
+    last_position_of_y = e.touches[0].clientY - canvas.offsetTop;
+
+}
+
+canvas.addEventListener("touchmove", my_touchmove);
+function my_touchmove(e)
+{
+   
+    current_position_of_touch_x = e.touches[0].clientX - canvas.offsetLeft;
+    current_position_of_touch_y = e.touches[0].clientY - canvas.offsetTop;
+    
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+    ctx.lineWidth = width_of_line;
+
+    
+    ctx.moveTo(last_position_of_x, last_position_of_y);
+
+  
+    ctx.lineTo(current_position_of_touch_x,current_position_of_touch_y);
+    ctx.stroke();
+
+    last_position_of_x = current_position_of_touch_x;
+    last_position_of_y = current_position_of_touch_y;
+}
+
+    
